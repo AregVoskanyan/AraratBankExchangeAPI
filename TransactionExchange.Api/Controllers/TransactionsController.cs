a3e683fb74cafc39da82a2929c4fadc73d3c334f
Ernest Manukyan <99879289+ernestmanukyan1@users.noreply.github.com>
2023-02-11 15:23:44 +0400

    Init controller

---
+++ TransactionExchange.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TransactionExchange.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateAsync()
+        {
+            return Ok("");
+        }
+
+        [HttpGet("List")]
+        public async Task<IActionResult> GetListAsync()
+        {
+            return Ok("");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetListAsync(int id)
+        {
+            return Ok("");
+        }
+    }
+}
---
+++ TransactionExchange.Api/Services/TransactionService.cs
@@ -0,0 +1,8 @@
+﻿using TransactionExchange.Api.Services.Interfaces;
+
+namespace TransactionExchange.Api.Services
+{
+    public class TransactionService : ITransactionService
+    {
+    }
+}
